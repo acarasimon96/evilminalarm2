ad07bbc3dcd9b7b2665347c31fd3564dd562bd2d
Timothy Bautista <yule2000@live.com>
2023-09-21 20:41:27 -0600

    Load config before splash text, print sounds dir path in debug mode

--- evilminalarm/__main__.py
+++ evilminalarm/__main__.py
@@ -8,10 +8,12 @@ from colorama import Fore, Style
 from . import __version__
 from .core import Core
 from .logging import debug_print, tee
-from .settings import load_config
+from .settings import config, load_config
 
 
 def main():
+    load_config()
+
     tee(
         f"{Fore.RED}{Style.BRIGHT}"
         f"Evil Minutes of the Clock Alarms 2 v{__version__}\n"
@@ -20,9 +22,11 @@ def main():
         "Press Ctrl+C or send a SIGINT to this process to exit\n"
         f"{Style.RESET_ALL}"
     )
-    load_config()
+    debug_print(f"Using sounds directory: {config['sounds_dir']}")
+
     Core().run()
-    debug_print(f"+++ Exited properly at {datetime.now().strftime('%H:%M:%S')} +++")
+
+    debug_print(f"+++ Exited properly at {datetime.now().strftime('%H:%M:%S')} +++\n")
 
 
 if __name__ == "__main__":
